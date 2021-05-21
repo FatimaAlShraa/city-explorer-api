f6aa5580a2de3cc80144bb80158dcafc32722fac
FatimaAlShraa <falhmood66@gmail.com>
2021-05-21 12:28:17 +0300

    finish backend

---
+++ server.js
@@ -0,0 +1,29 @@
+'use strict'
+const express = require ('express');
+const server =express();
+const weatherData =require('./weather.json')
+const PORT = 3001
+
+// server.get('/' , (req , res)=>{
+//     let test='hello from me'
+//     res.send(test)
+// })http://localhost:3001/getweather?city_name=Amman
+server.get('/getWeather', (req, res) => {
+    console.log(req.query)
+    let weatherCity= req.query.city_name
+    let wetherItem = weatherData.find(item => {
+        if (item.city_name == weatherCity)
+            return item
+    })
+    res.send(wetherItem)
+})
+
+
+server.get('*', (req, res) => {
+    res.status(500).send('the weather for this city is not found');
+})
+
+
+server.listen(PORT, () =>{
+    console.log(`listiningg on PORT ${PORT}`)
+})
